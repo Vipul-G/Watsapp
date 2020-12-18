155a919950ba7de242c2a186f610f68b338d0628
Vipul-G <wtech905@gmail.com>
2020-12-18 18:30:47 +0530

    debug

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -3,9 +3,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import io from 'socket.io-client'
 
 const SocketContext = React.createContext()
-const backendURL = process.env.REACT_APP_LOCAL ? 'http://localhost:9000' : 'https:stormy-forest-22894.herokuapp.com';
-
-console.log({backendURL})
+const backendURL = process.env.REACT_APP_LOCAL ? 'http://localhost:9000' : 'https://stormy-forest-22894.herokuapp.com';
 
 export function useSocket () {
   return useContext(SocketContext)
