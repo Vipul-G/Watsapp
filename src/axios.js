d264cb360ba53705517fb1fcf9ef4fb0b341ddc9
Vipul-G <wtech905@gmail.com>
2020-12-18 17:51:54 +0530

    set hosting URL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'http://localhost:9000/api/v1' // http server address
-});
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9000/api/v1' 
+: 'https://stormy-forest-22894.herokuapp.com/api/v1' 
+const instance = axios.create({baseURL});
 
 export default instance;
